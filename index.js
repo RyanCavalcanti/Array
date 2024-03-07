948ea75448f8275685e64c24e4d672cbb1309c6a
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-03-07 15:23:38 -0300

    first commit array

---
+++ index.js
@@ -0,0 +1,14 @@
+let nomes = []
+let continuar = true;
+let posicaDoArray = 0;
+
+while(continuar){
+    let nomeInserido = prompt("Insira seu nome");
+    nomes[posicaDoArray] = nomeInserido;
+
+    let desejaContinuar = prompt("Insira 1 caso deseje adicionar um novo nome")
+    if(desejaContinuar != '1'){
+        continuar = false;
+    }
+    posicaDoArray++;
+}
